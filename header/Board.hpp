9228dbd9f304547606be1256a1b9a84bf87f7f8d
alexander7862 <ktran945@gmail.com>
2021-02-24 00:01:21 -0800

    Board header file, first version

---
+++ header/Board.hpp
@@ -0,0 +1,65 @@
+//Board Class header file
+#ifndef _BOARD_HPP_
+#define _BOARD_HPP_
+
+#include "../src/Piece.cpp"
+
+//class def of square with member function definitions
+class Square {
+  private:
+	 Piece p;
+	 int x;
+	 int y;
+  public: //member functions w/ definitions
+	Square(int ex, int why, Piece p)
+	{
+		this->setPiece(p);
+		this->setXpos(ex);
+		this->setYpos;
+	}
+
+	Piece getPiece()
+	{
+		return p;
+	
+	}
+	void setPiece(Piece piece)
+	{ 
+	    p = piece;	
+	}
+	void setXpos(int xOne)
+	{
+		x = xOne;
+	}
+	void setYpos(int yOne)
+	{
+		y = yOne;
+	}
+
+	int getXpos()
+	{
+		return x;
+	}
+	int getYpos()
+	{
+		return y;
+	}
+
+
+}
+
+class Board{
+   private:
+	Square cells[8][8];
+ 
+
+   public:
+	   Board();
+	   Square getCells();
+	   void setBoard();
+}
+
+
+
+
+#endif
