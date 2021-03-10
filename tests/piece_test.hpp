1cb01e4e4b78902e0d9dc2d42ab6523ead7c0bc3
aa22510759 <totesanders@gmail.com>
2021-03-10 09:43:17 -0800

    added piece test

--- tests/piece_test.hpp
+++ tests/piece_test.hpp
@@ -1,30 +1,97 @@
+#ifndef _PIECE_TEST_HPP_
+#define _PIECE_TEST_HPP_
+
 #include "gtest/gtest.h"
-#include "../src/pawn_piece.cpp"
-#include "../src/piece.cpp"
+//#include "../src/Board.cpp"
+//#include "../src/piece.cpp"
 
-TEST(PieceTest, testCapture)
-{	
-	Piece * temp = new Pawn(2, 2);
-	
-	pos dest;
-	dest.x = 2;
-	dest.y = 2;
-	dest.occupied = true;
-	
-	EXPECT_TRUE(temp->capture(dest));
-	delete temp;
-}
+#include <iostream>
+#include <string>
 
-TEST(PieceTest, testCaptureFailed)
+
+
+
+TEST (pieceTest, pawnTest)
 {
-        Piece * temp = new Pawn(2, 2);
+	std::string pos = "c2";
 
-        pos dest;
-        dest.x = 2;
-        dest.y = 2;
-        dest.occupied = false;
-
-        EXPECT_FALSE(temp->capture(dest));
-        delete temp;
+	Position move;
+        move.xpos = (int) pos[0] - 97;
+	move.ypos = atoi(&pos[1]) - 1;
+	
+	Pawn * p = new Pawn(White,move);
+		
+	EXPECT_EQ(p->getVal(), "P");
+	delete p;
 }
 
+TEST (pieceTest, knightTest)
+{
+        std::string pos = "g1";
+
+        Position move;
+        move.xpos = (int) pos[0] - 97;
+        move.ypos = atoi(&pos[1]) - 1;
+
+        Knight * n = new Knight(White,move);
+
+        EXPECT_EQ(n->getVal(), "N");
+        delete n;
+}
+
+TEST (pieceTest, kingTest)
+{
+        std::string pos = "e1";
+
+        Position move;
+        move.xpos = (int) pos[0] - 97;
+        move.ypos = atoi(&pos[1]) - 1;
+
+        King * k = new King(White,move);
+
+        EXPECT_EQ(k->getVal(), "K");
+        delete k;
+}
+
+TEST (pieceTest, rookTest)
+{
+        std::string pos = "a1";
+
+        Position move;
+        move.xpos = (int) pos[0] - 97;
+        move.ypos = atoi(&pos[1]) - 1;
+
+        Rook * r = new Rook(White,move);
+
+        EXPECT_EQ(r->getVal(), "R");
+        delete r;
+}
+
+TEST (pieceTest, bishopTest)
+{
+        std::string pos = "d1";
+
+        Position move;
+        move.xpos = (int) pos[0] - 97;
+        move.ypos = atoi(&pos[1]) - 1;
+
+        Bishop * b = new Bishop(White,move);
+
+        EXPECT_EQ(b->getVal(), "B");
+        delete b;
+}
+
+TEST (pieceTest, queenTest)
+{
+        std::string pos = "f1";
+
+        Position move;
+        move.xpos = (int) pos[0] - 97;
+        move.ypos = atoi(&pos[1]) - 1;
+
+        Queen * q = new Queen(White,move);
+
+        EXPECT_EQ(q->getVal(), "Q");
+        delete q;
+}
+#endif
